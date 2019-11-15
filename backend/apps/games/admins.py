b1834f87e30010d2ef2a329e57c256540bfa9071
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2019-11-15 20:20:52 +0100

    improve flask admin

--- backend/apps/games/admins.py
+++ backend/apps/games/admins.py
@@ -1,5 +1,4 @@
 from flask_admin.contrib.sqla import ModelView
-from flask_admin.form.fields import Select2TagsField
 
 from apps.extensions import (
     admin,
@@ -9,20 +8,8 @@ from apps.extensions import (
 from . import models
 
 
-class Select2StrTagsField(Select2TagsField):
-    def process_formdata(self, valuelist):
-        super().process_formdata(valuelist)
-        self.data = [str(x) for x in self.data]
+class QuestionModelView(ModelView):
+    inline_models = (models.Answer,)
 
-
-class QuestionView(ModelView):
-    def scaffold_form(self):
-        form_class = super().scaffold_form()
-        form_class.options = Select2StrTagsField(
-            'Options',
-            save_as_list=True,
-        )
-        return form_class
-
-
-admin.add_view(QuestionView(models.Question, db.session))
+admin.add_view(ModelView(models.Answer, db.session))
+admin.add_view(QuestionModelView(models.Question, db.session))
--- backend/apps/games/models.py
+++ backend/apps/games/models.py
@@ -1,18 +1,49 @@
 from flask import current_app
 
 from sqlalchemy import (
+    Boolean,
     Column,
     Float,
-    Integer,
+    ForeignKey,
     String,
 )
-from sqlalchemy.dialects.postgresql import ARRAY
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import relationship
 
 from apps.common.models import UUIDable
 
 
 class Question(UUIDable):
-    text = Column(String)
-    reward = Column(Float)
-    options = ARRAY(String(length=255), as_tuple=True)
-    correct_answer = Column(Integer)
+    text = Column(String, nullable=False)
+    reward = Column(Float, default=1, nullable=False)
+
+    options = relationship(
+        'Answer',
+        back_populates='question',
+        lazy='selectin',
+    )
+
+    def __str__(self) -> str:
+        return f'{self.text} - {self.reward}$'
+
+    @property
+    def correct_answer(self) -> 'Answer':
+        return next(answer for answer in self.options if answer.is_correct)
+
+
+class Answer(UUIDable):
+    text = Column(String(length=255), nullable=False)
+    is_correct = Column(Boolean, default=False)
+    question_id = Column(
+        UUID(as_uuid=True),
+        ForeignKey(f'{Question.__tablename__}.id'),
+        nullable=False,
+    )
+    question = relationship(
+        'Question',
+        back_populates='options',
+    )
+
+    def __str__(self) -> str:
+        prefix = '✓ ' if self.is_correct else ''
+        return f'{prefix}{self.text}'
