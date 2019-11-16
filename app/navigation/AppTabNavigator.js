c022f7f70fa8b68c77ae79bbff72767bdba87e51
bbarwik <bbarwik@gmail.com>
2019-11-16 09:33:39 +0100

    Fixed small ios icon bug

--- app/navigation/AppTabNavigator.js
+++ app/navigation/AppTabNavigator.js
@@ -32,7 +32,7 @@ HomeStack.navigationOptions = {
       focused={focused}
       name={
         Platform.OS === 'ios'
-          ? `ios-home${focused ? '' : '-outline'}`
+          ? 'ios-home'
           : 'md-home'
       }
     />
