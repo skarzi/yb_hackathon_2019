8f947b5c4a1ec56b3fab90e0e7d8fb436b5d0c70
Łukasz Skarżyński <skarzynski_lukasz@protonmail.com>
2019-11-16 03:43:27 +0100

    clean backend project
    Remove unused dependencies and docker-compose services.

--- backend/apps/extensions.py
+++ backend/apps/extensions.py
@@ -5,15 +5,10 @@ from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-from .task_app.extensions import Celery
 
 db = SQLAlchemy()
 migrate = Migrate(db=db)
 schemas = Marshmallow()
-celery_extension = Celery()
 jwt = JWTManager()
-# TODO: uncomment for socketio support
-# from .common.extensions.socketio import SocketIO
-# socketio = SocketIO()
 admin = Admin()
 cors = CORS()
